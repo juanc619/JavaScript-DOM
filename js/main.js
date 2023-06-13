550bf35d1a7ad3d7f8970e3da9b3848b048a9477
Juan carlos <juancarlosvelasquez619@gmail.com>
2023-06-13 01:18:06 -0500

    querySelector, querySelectoAll

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// const contenido = document.getElementById("contenido");
+// const parrafos = document.getElementsByClassName("parrafo");
+
+const contenido = document.querySelector("#contenido");
+const parrafos = document.querySelectorAll(".parrafo");
+
+
+// console.log(parrafos);
+// parrafos.forEach(parrafo => console.log(parrafo));
+console.log(parrafos[0]);
